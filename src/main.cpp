0b93b193441964c9228d931503c79cbef3b8bee1
BengtHolm <bengt.s.holm@gmail.com>
2015-10-12 00:29:52 +0200

    First main Pointer Container example

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+struct ptr_less
+{
+	bool operator() ( const T* a, const T* b )
+	{
+		return std::less<T>()( *a, *b );
+	}
+};
+
+std::vector<int> v;
+std::vector<int*> p;
+
+void initialize()
+{
+	v.push_back( 3 );
+	v.push_back( 2 );
+	v.push_back( 8 );
+	v.push_back( 4 );
+	v.push_back( 3 );
+	v.push_back( 9 );
+
+	for( std::vector<int>::iterator it = v.begin(); it!=v.end(); ++it )
+	{
+		p.push_back( &*it );
+	}
+}
+
+void sortAndPrint()
+{
+	std::sort( p.begin(), p.end(), ptr_less<int>() );
+
+	for( std::vector<int*>::iterator it = p.begin(); it!=p.end(); ++it )
+	{
+		std::cout << **it << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	initialize();
+	sortAndPrint();
+
+	return 0;
+}
